a23f5df69167ed041870c8dbb96e1b9e67cdcdfd
VarunGoud04 <bandivarungoud1998@gmail.com>
2025-04-23 14:57:11 +0530

    bcrypt update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { auth, db } = require('./firebaseConfig');
 const { createUserWithEmailAndPassword, signInWithEmailAndPassword } = require('firebase/auth');
 const { doc, setDoc, getDoc } = require('firebase/firestore');
